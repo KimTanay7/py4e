fac834e1b545ae6e22c18bdc3572af8084583e4e
arvinjason <arvinjasonaquino@gmail.com>
2020-03-03 11:14:18 +0800

    Celsius to Fahrenheit

---
+++ Temperature.py
@@ -0,0 +1,8 @@
+print ("Temperature Calculator: Celsius to Fahrenheit")
+name = input("Name:")
+print ("Hello ",name, "!")
+print ("*********************************")
+x= input(" Enter Celsius:")
+Celsius = float(x)
+Fahrenheit =(float(x) * 9/5)+ 32
+print ("Fahrenheit:",Fahrenheit)
